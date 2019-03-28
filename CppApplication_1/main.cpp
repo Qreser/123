147657db48f95822705df6cbd81ec16d42dd6520
Szymon Kisielewski <szymon.kisielewski1@gmail.com>
2019-03-28 19:31:52 +0100

    d

---
+++ CppApplication_1/main.cpp
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/* 
+ * File:   main.cpp
+ * Author: student
+ *
+ * Created on 28 marca 2019, 19:30
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+    cout<<"siemanko"<<endl;
+    return 0;
+}
+
